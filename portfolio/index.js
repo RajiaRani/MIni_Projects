0ddf07cd1e8328309acb76bd47a1788feceb883d
Rajia Rani <rajiarani.syal@gmail.com>
2024-04-18 13:22:46 -0500

    added

--- portfolio/index.js
+++ portfolio/index.js
@@ -3,3 +3,7 @@ const app = express();
 const path = require("path");
 
 app.use(express.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set(express.static(path.join(__dirname, "public")));
