739407d4c145256d1f0b911cf705270c73b3dc75
Rajia Rani <rajiarani.syal@gmail.com>
2024-04-18 15:04:10 -0500

    added

--- portfolio/index.js
+++ portfolio/index.js
@@ -8,6 +8,10 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.set(express.static(path.join(__dirname, "public")));
 
+app.get("/portfolio",(req,res) => {
+ res.render("index.ejs");
+});
+
 app.listen(8080, () => {
-    res.send("I am listening on port 8080.");
+    console.log("I am listening on port 8080.");
 });
